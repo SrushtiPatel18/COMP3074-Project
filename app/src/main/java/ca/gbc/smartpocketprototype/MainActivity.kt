6950c21637027d4a68470592e2bd9b5abffd7b61
Srushti Patel <srushtimpatel007@gmail.com>
2025-10-06 11:41:25 -0400

    Updated AndroidManifest.xml

--- app/src/main/java/ca/gbc/smartpocketprototype/MainActivity.kt
+++ app/src/main/java/ca/gbc/smartpocketprototype/MainActivity.kt
@@ -32,3 +32,19 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SmartPocketPrototypeTheme {
+        Greeting("Android")
+    }
+}
